cf5b305875a8e610e2b6f0389290466b0f397060
marunko <marunkopavlo@gmail.com>
2023-09-12 21:00:10 +0300

    f2

--- src/AlgorithmsArrays/Search/BinarySearch.java
+++ src/AlgorithmsArrays/Search/BinarySearch.java
@@ -1,7 +1,16 @@
 package AlgorithmsArrays.Search;
 
 public class BinarySearch {
+    static int[] array = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
     public static void main(String[] args) {
+        binarySearch(array);
+    }
+    public static void binarySearch(int [] array){
 
+        int right, left=0, middle=right=2;
+        System.out.print(middle);
+        while (right > left){
+
+        }
     }
 }
