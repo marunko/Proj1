4ca4acb0eb254dc9bd4a3c4679fb41f9f8772ed0
marunko <marunkopavlo@gmail.com>
2023-09-23 19:23:36 +0300

    f3

--- src/AlgorithmsArrays/Search/BinarySearch.java
+++ src/AlgorithmsArrays/Search/BinarySearch.java
@@ -3,14 +3,34 @@ package AlgorithmsArrays.Search;
 public class BinarySearch {
     static int[] array = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
     public static void main(String[] args) {
-        binarySearch(array);
+        binarySearch(array, 0);
+        binarySearch(array, 9);
+        binarySearch(array, 10);
+        binarySearch(array, 6);
     }
-    public static void binarySearch(int [] array){
+    public static void binarySearch(int [] array, int number){
 
-        int right, left=0, middle=right=2;
-        System.out.print(middle);
-        while (right > left){
+        int right = array.length-1, left=0, middle;
 
+        while (right >= left){
+
+            middle = (right+left)/2;
+            if(number > array[middle]){
+                left = middle+1;
+            }
+            else if(number < array[middle]){
+                right = middle-1;
+            }
+            else{
+                System.out.printf("The number found on index: " + middle + " number: "+array[middle]);
+                return;
+            }
         }
+        System.out.print("Num has not been found");
     }
+
+    public static void binarySearch2(){
+
+    }
+
 }
